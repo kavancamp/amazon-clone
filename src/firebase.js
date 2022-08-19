d9536c81d5068a4b8eb91a49ee26303c03117f28
Keenah <kvancamp@mail.com>
2022-08-19 18:12:44 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMVpKWujYGfk3LSzHbiskgppi9LvkPyhc",
+  authDomain: "clone-946c0.firebaseapp.com",
+  projectId: "clone-946c0",
+  storageBucket: "clone-946c0.appspot.com",
+  messagingSenderId: "516515981621",
+  appId: "1:516515981621:web:3e925191106ac3921c047c"
+};
+
