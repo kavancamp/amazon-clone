0753ab5ec86155ff4a0c4749a09a521ee01ba61a
Keenah <kvancamp@mail.com>
2022-08-20 18:45:36 -0500

    added create and login authentication

--- src/Login.js
+++ src/Login.js
@@ -1,40 +1,81 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './Login.css';
+import "./Login.css";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        navigate("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+  const register = (e) => {
+    e.preventDefault();
+    auth
+    .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
-    <div className='login'>
-      <div>
-        <Link to='/'>
-            <img
-                className="login__logo"
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt=""
-            />
-        </Link>
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={signIn}
+            type="submit"
+            className="login__signInButton"
+          >
+            {" "}
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
       </div>
-        <div className='login__container'>
-            <h1>Sign-in</h1>
-
-            <form>
-                <h5>E-mail</h5>
-                <input type='text'  />
-
-                <h5>Password</h5>
-                <input type='password'  />
-
-                <button type='submit'  className='login__signInButton'>Sign In</button>
-            </form>
-
-            <p>
-                By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-            </p>
-
-            <button  className='login__registerButton'>Create your Amazon Account</button>
-        </div>
     </div>
-)
+  );
 }
 
-export default Login
+export default Login;
