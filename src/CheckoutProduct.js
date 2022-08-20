ff69ad7b38ac49713364dfde88da37333d01d760
Keenah <kvancamp@mail.com>
2022-08-20 13:29:41 -0500

    added dispatch

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import './CheckoutProduct.css';
-
+import {useStateValue} from "./StateProvider";
 
 function CheckoutProduct({id, image, title, price, rating}) {
+  const [{ basket}, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // remove the item from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
   return (
     <div className='checkoutProduct'>
       <img src={image} alt="" className="checkoutProduct__image" />
@@ -19,7 +27,9 @@ function CheckoutProduct({id, image, title, price, rating}) {
           .map((_, i) => (
             <p>🌟</p>
           ))}
-          <button></button>
+         
+          <button onClick={removeFromBasket}>Remove from Basket</button>
+       
         </div>
       </div>
     </div>
