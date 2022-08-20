93b5eff0bd6f072490440da45e2c128318c5b11c
Keenah <kvancamp@mail.com>
2022-08-20 13:29:27 -0500

    added basked info

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from './Subtotal';
+import {useStateValue} from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct.js";
+import "./CheckoutProduct.css";
 
 function Checkout() {
+  const[{basket}, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,7 +19,15 @@ function Checkout() {
           <h2 className="checkout__title">
             Your Basket
           </h2>
-
+          {basket.map(item => (
+            <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
