13d3b6f7d297ec02a67f1e13a90eabdde8645aaa
Keenah <kvancamp@mail.com>
2022-08-20 10:24:25 -0500

    imported fontawesome

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMagnifyingGlass, faCartShopping} from "@fortawesome/free-solid-svg-icons";
+import {useStateValue} from "./StateProvider";
+library.add(faMagnifyingGlass, faCartShopping);
 //import { MagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 function Header() {
+  const [{basket}, dispatch] = useStateValue();
   return (
     <div className="header">
       <Link to="/">
@@ -32,7 +37,7 @@ function Header() {
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLinetwo">Orders</span>
+          <span className="header__optionLinetwo">& Orders</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
@@ -41,7 +46,7 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
