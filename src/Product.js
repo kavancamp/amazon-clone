db3d511448980f8c9f00bc76f20ccf268d3189ba
Keenah <kvancamp@mail.com>
2022-08-19 20:19:04 -0500

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -18,10 +35,11 @@ function Product({ title, image, price, rating }) {
             ))}
         </div>
       </div>
+
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
-    
   );
 }
 
