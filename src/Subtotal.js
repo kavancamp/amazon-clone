1a140d0b590ee9a7b0d00e1fdb9854da318601a9
Keenah <kvancamp@mail.com>
2022-08-19 20:18:54 -0500

    Update Subtotal.js

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import './Subtotal.css';
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
+  const navigate = useNavigate();
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
-    <div className="subtotal">Subtotal</div>
-  )
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)} // Part of the homework
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button
+        disabled={basket.length === 0}
+        onClick={(e) => navigate.push("/payment")}
+      >
+        Proceed to Checkout
+      </button>
+    </div>
+  );
 }
 
-export default Subtotal
+export default Subtotal;
