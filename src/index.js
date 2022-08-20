b5b8e8165e715aa1adb5a940f47bd906156cfb03
Keenah <kvancamp@mail.com>
2022-08-19 20:19:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import StateProvider from "./StateProvider";
+import reducer, { initialState } from "./reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      {" "}
+      <App />
+    </StateProvider>
   </BrowserRouter>
 );
 
