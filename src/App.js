49182795bb3dcfafee2ac116039e3983bcba5a98
Keenah <kvancamp@mail.com>
2022-08-19 20:18:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import "./App.css";
-import React from "react";
-import { Routes, Route } from "react-router";
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import Checkout from "./Checkout.js";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Header />
+
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} >
+          <Home />
+          </Route>
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
