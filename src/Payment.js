f0285bd63975e8f2e816944eddcc6462fb0d5990
Keenah <kvancamp@mail.com>
2022-08-21 16:21:00 -0500

    Update Payment.js

---
+++ src/Payment.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Payment.css";
+
+function Payment() {
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <div className="payment__title">
+          <h3>Delivery Address</h3>
+        </div>
+      
+      <div className="payment__address">
+        <p></p>
+      </div>
+       </div>
+      <div className="payment__section">
+        
+      </div>
+   
+    </div>
+  )
+}
+
+export default Payment
