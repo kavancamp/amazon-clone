c73bec2c985ea9c7b62ee60887d35043819714fb
Keenah <kvancamp@mail.com>
2022-08-21 16:31:46 -0500

    added props

--- src/Payment.js
+++ src/Payment.js
@@ -1,24 +1,43 @@
 import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Payment() {
   const [{basket, user}, dispatch] = useStateValue();
-  
+
   return (
     <div className="payment">
       <div className="payment__container">
         <div className="payment__title">
           <h3>Delivery Address</h3>
         </div>
-
         <div className="payment__address">
           <p>{user?.email}</p>
           <p>123 redman</p>
           <p>green bay</p>
         </div>
       </div>
-      <div className="payment__section"></div>
+
+      <div className="payment__section">
+        <div className="payment__title">
+          <h3>Review cart and delivery</h3>
+        </div>
+        <div className="payment__items">
+        {basket.map(item => (
+            <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="payment__section">
+
+      </div>
     </div>
   );
 }
