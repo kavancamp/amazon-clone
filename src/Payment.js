d0be9ef7ec3e596750250e5e4f1d361e8eee964f
Keenah <kvancamp@mail.com>
2022-08-21 16:24:58 -0500

    import useStateValue

--- src/Payment.js
+++ src/Payment.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
 
 function Payment() {
+  const [{basket, user}, dispatch] = useStateValue();
+  
   return (
     <div className="payment">
       <div className="payment__container">
         <div className="payment__title">
           <h3>Delivery Address</h3>
         </div>
-      
-      <div className="payment__address">
-        <p></p>
+
+        <div className="payment__address">
+          <p>{user?.email}</p>
+          <p>123 redman</p>
+          <p>green bay</p>
+        </div>
       </div>
-       </div>
-      <div className="payment__section">
-        
-      </div>
-   
+      <div className="payment__section"></div>
     </div>
-  )
+  );
 }
 
-export default Payment
+export default Payment;
