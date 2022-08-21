2278a69b2e365072db7a2c09598e5ef0e66f5ca9
Keenah <kvancamp@mail.com>
2022-08-20 20:30:46 -0500

    Update Orders.js

---
+++ src/Orders.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
+import "./Orders.css";
+import { useStateValue } from "./StateProvider";
+import Order from "./Order";
+
+function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
+  return (
+    <div className="orders">
+      <h1>
+        Your Orders <small>({orders.length} orders)</small>
+      </h1>
+      {!user && <h2>Please sign in to see your orders</h2>}
+      {user && orders.length === 0 && (
+        <h2>You currently have no orders placed</h2>
+      )}
+
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
