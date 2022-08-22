a5dfb55cd856d7d0f6b600fd3dd113c99e0f925c
Keenah <kvancamp@mail.com>
2022-08-22 17:43:10 -0500

    Update Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -1,52 +1,149 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Payment.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { useElements, CardElement, useStripe } from "@stripe/react-stripe-js";
+import { getBasketTotal } from "./reducer";
+import axios from "./axios";
+import CurrencyFormat from "react-currency-format";
+import { db } from "./firebase";
 
 function Payment() {
-  const [{basket, user}, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+  const navigate = useNavigate();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  // state
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  console.log("DEBUG", clientSecret);
+
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        /* Show a success message upon completion */
+        /* https://dashboard.stripe.com/test/payments */
+        console.log(paymentIntent);
+
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
+
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        dispatch({
+          type: "EMPTY_BASKET",
+        });
+
+        navigate.replace("/orders");
+      });
+  };
 
   return (
     <div className="payment">
       <div className="payment__container">
-        <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
         <div className="payment__section">
-        <div className="payment__title">
-          <h3>Delivery Address</h3>
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
         </div>
-        <div className="payment__address">
-          <p>{user?.email}</p>
-          <p>123 redman</p>
-          <p>green bay</p>
-        </div>
-      </div>
 
-      <div className="payment__section">
-        <div className="payment__title">
-          <h3>Review cart and delivery</h3>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__address">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
-        <div className="payment__address">
-        {basket.map(item => (
-            <CheckoutProduct
-            id={item.id}
-            title={item.title}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="payment__section">
+
+        <div className="payment__section">
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
           <div className="payment__details">
             <h4>Card Details</h4>
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
 
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                      <h3>Order Total: {value}</h3>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)} // Part of the homework
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy now"}</span>
+                </button>
+              </div>
+
+              {error && <div>{error}</div>}
+            </form>
           </div>
-      </div>
+        </div>
       </div>
     </div>
   );
