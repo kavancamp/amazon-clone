d673650b3b764042d18f5b249d6b91921370f4fd
Keenah <kvancamp@mail.com>
2022-08-25 11:48:26 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // URL for deployed cloudfunction
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-946c0/us-central1/api "
- 
+  baseURL: "http://localhost:5001/clone-946c0/us-central1/api"
+  
 });
 
 export default instance;
