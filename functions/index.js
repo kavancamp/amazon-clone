567b93de25d29cba290c7cea32589c2edc0746f9
Keenah <kvancamp@mail.com>
2022-08-25 11:48:17 -0500

    initiated functions

--- functions/index.js
+++ functions/index.js
@@ -4,31 +4,33 @@ const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")("sk_test_51LZMtxLL5LVXIliA1rWURELZV6GD4GKc7h0nBDGR3IyUbQrRApxH4S4wF27UszLXCLGOrMCreB2RQ81sxtKE1Y9n00NOSui3GK");
 
-// API
-
 // App config
 const app = express();
-// Middlewares
+
+// middlewares
 app.use(cors({origin: true}));
 app.use(express.json());
 
-// API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.post('/payments/create', async (request, response) => {
-  const total = request.query.total;
-  console.log('Payment request recieved. For this amount >> ' total)
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  console.log("GOT A REQUEST");
+  console.log(total);
+
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total,
-    currency: "usd"
+    currency: "usd",
   });
-  response.status(201).send({
+
+  // 201 = Payment Created
+  res.status(201).send({
     clientSecret: paymentIntent.client_secret,
-    
-  }) //okay, created
+  });
 });
 
-// Listen
+// listen
 exports.api = functions.https.onRequest(app);
 
 /* http://localhost:5001/clone-946c0/us-central1/api */
