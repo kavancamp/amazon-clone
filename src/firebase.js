579690c639e3fe474312861835312777033d0439
Keenah <kvancamp@mail.com>
2022-09-10 14:14:21 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,18 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
+const { initializeApp } = require("firebase/app");
+const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
+
+const app = initializeApp({
+  // Your firebase configuration object\  
+  apiKey: "AIzaSyCMVpKWujYGfk3LSzHbiskgppi9LvkPyhc",
+  authDomain: "clone-946c0.firebaseapp.com",
+  projectId: "clone-946c0",
+  storageBucket: "clone-946c0.appspot.com",
+  messagingSenderId: "516515981621",
+  appId: "1:516515981621:web:3e925191106ac3921c047c"
+});
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMVpKWujYGfk3LSzHbiskgppi9LvkPyhc",
@@ -11,6 +23,17 @@ const firebaseConfig = {
   messagingSenderId: "516515981621",
   appId: "1:516515981621:web:3e925191106ac3921c047c"
 };
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LcAIushAAAAAJiHOgrufhUeF-UBYdVoEsPyu_us'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
